63c3105b8219357193e3d2a04ca8a4c0d68ece92
oliha <poliha2002@yahoo.com>
2019-04-29 14:40:09 +0100

    fix indentation

--- clients/horizonclient/main.go
+++ clients/horizonclient/main.go
@@ -1,6 +1,5 @@
 /*
- Package horizonclient provides client access to a Horizon server, allowing an application to post
- transactions and look up ledger information.
+Package horizonclient provides client access to a Horizon server, allowing an application to post transactions and look up ledger information.
 
 This library provides an interface to the Stellar Horizon service. It supports the building of Go applications on
 top of the Stellar network (https://www.stellar.org/). Transactions may be constructed using the sister package to
